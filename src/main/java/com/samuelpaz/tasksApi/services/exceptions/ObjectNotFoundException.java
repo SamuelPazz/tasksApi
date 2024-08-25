2c855999d54e8ebf16229171583cdd06899f7031
SamuelPazz <samuel.paz@sptech.school>
2024-08-24 23:35:36 -0300

    Add dependencies and create SecurityConfig

--- src/main/java/com/samuelpaz/tasksApi/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/samuelpaz/tasksApi/services/exceptions/ObjectNotFoundException.java
@@ -1,15 +1,15 @@
-package com.samuelpaz.tasksApi.services.exceptions;
+    package com.samuelpaz.tasksApi.services.exceptions;
 
-import jakarta.persistence.EntityNotFoundException;
+    import jakarta.persistence.EntityNotFoundException;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ObjectNotFoundException extends EntityNotFoundException {
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public class ObjectNotFoundException extends EntityNotFoundException {
 
-    public ObjectNotFoundException(String message) {
-        super(message);
+        public ObjectNotFoundException(String message) {
+            super(message);
+        }
+
     }
-
-}
